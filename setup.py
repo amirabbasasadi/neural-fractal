11a7aebd0f31f154275eb40ddf19ece335100e8d
Amirabbas Asadi <amir137825@gmail.com>
2021-12-31 16:58:48 +0330

    package setup file

---
+++ setup.py
@@ -0,0 +1,18 @@
+import pathlib
+from setuptools import find_packages, setup
+
+HERE = pathlib.Path(__file__).parent
+
+setup(
+    name="nfractal",
+    version="0.1.0",
+    description="Create Fractals Using Complex-Valued Neural Networks!",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/amirabbasasadi/neural-fractal.git",
+    author="Amirabbas Asadi",
+    author_email="amir137825@gmail.com",
+    packages=find_packages(exclude=("docs",)),
+    include_package_data=True,
+    install_requires=["tqdm", "torch>=1.8.1"],
+)
