fe8c71e6e26ce31a32b7b791cd4bb6a9985f3bf4
Amirabbas Asadi <amir137825@gmail.com>
2022-01-04 08:43:48 +0330

    update setup.py to release 0.3.0

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 import pathlib
 from setuptools import find_packages, setup
 
+
 HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
 setup(
     name="nfractal",
-    version="0.2.0",
+    version="0.3.0",
     description="Create Fractals Using Complex-Valued Neural Networks!",
-    long_description="Create Fractals Using Complex-Valued Neural Networks!\n Visit the homepage for more information.",
+    long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/amirabbasasadi/neural-fractal.git",
     author="Amirabbas Asadi",
